5df1d330928b4c769286a19942c25da039b52a59
chen-grace-p4 <gchen20@stuy.edu>
2022-01-23 18:42:33 -0500

    working on selectclient

--- selectclient.c
+++ selectclient.c
@@ -2,7 +2,7 @@
 #include "networking.h"
 
 int main() {
-  int fd, listen_socket, maxfd, client;
+  int fd, server_socket, maxfd, server;
   //this is the fd_set that will be modified by select
   fd_set read_fds;
   //this is the full fd_set to keep a stable record
@@ -10,12 +10,13 @@ int main() {
   char buffer[BUFFER_SIZE];
 
   //make sure clients is totally empty
-  FD_ZERO( &clients );
-  server_socket = client_setup();
+  FD_ZERO( &full_fds );
+  server_socket = client_setup(TEST_IP);
   printf("server socket: %d\n", server_socket);
 
   //add the listening socket to the master set
-  FD_SET(server_socket, &clients);
+  FD_SET(server_socket, &full_fds);
+  //FD_SET(STDIN, &full_fds);
   //we have to keep track of the largest file descriptor for select
   //currently, listen_socket is the only, and therefore largest
   maxfd = server_socket;
@@ -27,40 +28,55 @@ int main() {
 
   	// puts ready descriptors into read_fds
     int i = select(maxfd + 1, &read_fds, NULL, NULL, NULL);
-    error_check_sock(i, "select", listen_socket);
+    error_check_sock(i, "select", server_socket);
     printf("select ready: %d\n", i);
 
     //all we know is a file descriptor is ready, we don't know which one
     //loop through read_fds for any available socket
     for (fd = 0; fd <= maxfd; fd++) {
-
       //will return true if the provided file descriptor is in read_fds
       if (FD_ISSET(fd, &read_fds)) {
+           //server socket ready means new message
+           printf("ready socket: %d\n", fd);
+           printf("server socket: %d\n", server_socket);
+           if (fd == server_socket) {
+              //DISPLAY NEW MESSAGE
+              read(server, buffer, sizeof(buffer));
+              printf("received: -%s-\n", buffer);
 
-        //listen socket ready means new client
-        if (fd == listen_socket) {
-          client = server_connect(listen_socket);
-          FD_SET(client, &clients);
+           }
+           //server socket not ready so go to stdin
+           else {
+             printf("Input: ");
+             fgets(buffer, sizeof(buffer), stdin);
 
-          //make sure to update maxfd
-          if (client > maxfd) maxfd = client;
-        }//new connections
+             //replace newline with terminating null
+             *strchr(buffer, '\n') = 0;
 
-        //listen socket not ready so existing client
-        else {
-          if (read(fd, buffer, sizeof(buffer)) ) {
-            
-            }
+             char temp[BUFFER_SIZE];
+             strcpy(temp, buffer);
 
-            //write(fd, buffer, sizeof(buffer));
-          }//data to be read
-          else {
-            //remove this descriptor from the full set
-            FD_CLR(fd, &clients);
-            close(fd);
-          }//close the connection
-        }//existing socket
-      }//descriptor is readhy
+             //strcpy(temp, strcat(username, ": "));
+             //strcpy(temp, strcat(temp, buffer));
+
+             //separate buffer into array and check first word for command
+             char * first = strtok(buffer, " ");
+             if (strcasecmp(first, "update") == 0) {
+               printf("updating to most recent messages\n");
+             } else {
+               //printf("sending: -%s-\n", buffer);
+          		//write(server, buffer, sizeof(buffer));
+               write(server, temp, sizeof(temp));
+             }
+
+             //removes stdin from read_fds
+             FD_CLR(fd, &read_fds);
+             //close(fd);
+          }//existing socket
+      }//file descriptor is readhy
+      else {
+
+      }
     }//loop through sockets
   }//forever
   close(server_socket);
